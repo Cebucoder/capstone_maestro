689da393b2a5933a8e9cfd2517ad573d8e61a822
Cebucoder <danielabellana187@gmail.com>
2024-01-28 20:35:43 +0800

    update data

--- js/data.js
+++ js/data.js
@@ -1,5 +1,5 @@
-
 var project_capstone = [];
+var currentIndex;
 
 // Function to fetch JSON data
 async function fetchData() {
@@ -7,38 +7,39 @@ async function fetchData() {
         const response = await fetch('data.json');
         const data = await response.json();
         project_capstone = data;
+        currentIndex = parseInt(localStorage.getItem('currentIndex')) || 0; // Retrieve from local storage
         GenerateTitle(); // Call GenerateTitle after fetching data
     } catch (error) {
         console.error('Error fetching data:', error);
     }
 }
 
+function saveCurrentIndexToLocalStorage() {
+    localStorage.setItem('currentIndex', currentIndex.toString());
+}
 
+function displayProject(index) {
+    var project = project_capstone[index];
+    document.getElementById('title').innerHTML = project.title || '';
+    document.getElementById('description').innerHTML = project.description;
+    document.getElementById('developer_name').innerHTML = project.developer_name;
 
+    var researchKeys = Object.keys(project);
+    researchKeys = researchKeys.filter(key => key !== 'title' && key !== 'description');
+    researchKeys.forEach((key, i) => {
+        document.getElementById(key).innerHTML = `${project[key]}`;
+        project.Methodology = project.Methodology.replace(/\n/g, '<br>');
+        project.Expected_Deliverables = project.Expected_Deliverables.replace(/\n/g, '<br>');
+        project.Timeline_and_Milestones = project.Timeline_and_Milestones.replace(/\n/g, '<br>');
+    });
 
-    var currentIndex = 0;
+    document.getElementById('currentIndexDisplay').innerHTML = currentIndex + 1;
+    document.getElementById('list-count').innerHTML = project_capstone.length;
 
-    function displayProject(index) {
-        var project = project_capstone[index];
-        document.getElementById('title').innerHTML = project.title || '';
-        document.getElementById('description').innerHTML = project.description;
-        document.getElementById('developer_name').innerHTML = project.developer_name;
+    saveCurrentIndexToLocalStorage(); // Save current index to local storage
+}
 
-        var researchKeys = Object.keys(project);
-        researchKeys = researchKeys.filter(key => key !== 'title' && key !== 'description');
-        researchKeys.forEach((key, i) => {
-            document.getElementById(key).innerHTML = `${project[key]}`;
-            project.Methodology = project.Methodology.replace(/\n/g, '<br>');
-            project.Expected_Deliverables = project.Expected_Deliverables.replace(/\n/g, '<br>');
-            project.Timeline_and_Milestones = project.Timeline_and_Milestones.replace(/\n/g, '<br>');
-        });
-            document.getElementById('currentIndexDisplay').innerHTML = currentIndex + 1;
-            document.getElementById('list-count').innerHTML = project_capstone.length;
-    }
-
-
-
-function GenerateTitle(){
+function GenerateTitle() {
     if (currentIndex < project_capstone.length) {
         displayProject(currentIndex);
         currentIndex++;
@@ -51,15 +52,3 @@ function GenerateTitle(){
 
 // Call fetchData to initiate fetching data
 fetchData();
-
-
-
-
-
-
-
-
-
-
-
-
